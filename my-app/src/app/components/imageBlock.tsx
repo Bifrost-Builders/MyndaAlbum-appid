9abb58e54f091bc5f0252349572f3dc46c5d0ea5
GabrielMani <gabriel.sigvaldason@gmail.com>
2024-05-03 00:01:08 +0000

    Blocks now shows loc, and little fix here and there

--- my-app/src/app/components/imageBlock.tsx
+++ my-app/src/app/components/imageBlock.tsx
@@ -1,28 +1,33 @@
 /* 
 
-    Exports block with location,title,image,date
+    Exports block with country,city,image,province
 
     *default value !(src)! . Not found
 
 */
 
-type ImageBlockProps = {
-    src: string,
-    location?: string,
-    title?: string,
-    date?: string,
 
-}
+type Details = {
+    country?: string;
+    city?: string;
+    province?: string;
+  };
+  
+  type ImageBlockProps = {
+    src: string;
+    info?: Details;
+  };
 
-export default function ImageBlock({src,location = "Not found",title = "Not found",date = "Not found"}: ImageBlockProps) {
+export default function ImageBlock({ src, info = {} }: ImageBlockProps) {
+    const { country = "Not found", city = "Not found", province = "Not found" } = info;
     
     return (
             <div
                 className="h-[200px] w-full m-auto bg-center bg-no-repeat bg-cover col-span-3 rounded-[5px] relative text-white max-sm:row-span-2 max-sm:col-span-1"
                 style={{ backgroundImage: `url(${src})` }}>
-                <h1 className="absolute top-1 left-2 font-semibold">{ location }</h1>
-                <h1 className="absolute bottom-6 text-2xl left-2 font-bold">{ title}</h1>
-                <p className="text-sm absolute bottom-2 left-2 font-semibold">{ date }</p>
+                <h1 className="absolute top-1 left-2 font-semibold">{ country }</h1>
+                <h1 className="absolute bottom-6 text-2xl left-2 font-bold">{ city}</h1>
+                <p className="text-sm absolute bottom-2 left-2 font-semibold">{ province }</p>
             </div>
     )
 
