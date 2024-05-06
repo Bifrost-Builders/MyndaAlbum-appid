a52e8105f5992073a675fc5053dc0cc0bbbc98da
Valdas Kaubrys <valdaskaubrys9@gmail.com>
2024-05-06 10:17:40 +0000

    Breyting: eyða ónotaðan Firebase Storage kóða

--- my-app/src/app/firebase/firebaseConfig.js
+++ my-app/src/app/firebase/firebaseConfig.js
@@ -1,5 +1,4 @@
 import { initializeApp } from 'firebase/app';
-import { getStorage } from 'firebase/storage';
 import { getDatabase } from "firebase/database";
 
 const apiKey = process.env.FIREBASE_API_KEY;
@@ -10,16 +9,11 @@ const firebaseConfig = {
     projectId: "verkefni-4-og-5-i-vefthroun",
     messagingSenderId: "769414348370",
     appId: "1:769414348370:web:89ddd7a7f5f3721952698b",
-    storageBucket: "verkefni-4-og-5-i-vefthroun.appspot.com",
     databaseURL: "https://verkefni-4-og-5-i-vefthroun-default-rtdb.firebaseio.com/"
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
 
-const storage = getStorage(firebaseApp);
-
 const database = getDatabase(firebaseApp);
-
-export { storage };
 
 export { database };
