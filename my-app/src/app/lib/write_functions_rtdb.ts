0791a28ca51a1a930b52d46d6c0eb1beb66668f5
Valdas Kaubrys <valdaskaubrys9@gmail.com>
2024-05-09 20:11:37 +0000

    Refactor write_functions_rtdb.ts and update database structure

--- my-app/src/app/lib/write_functions_rtdb.ts
+++ my-app/src/app/lib/write_functions_rtdb.ts
@@ -1,45 +1,61 @@
 import { getDatabase, ref, set } from "firebase/database";
 
+// USER WRITE FUNCTIONS -----------------------------------------------------------------------------------------------------------------------------
+
+// Function to create a new user
 export function createUser(username, displayname_value, age_value, email_value) {
-  set(ref(getDatabase(), 'User/' + username), {
+  set(ref(getDatabase(), 'user/' + username), {
     displayname: displayname_value,
     email: email_value,
     age : age_value
   });
 }
 
-// Function to write a user's age
+// Function to write/change a user's age
 export function writeUserAge(username, age) {
-    const ageRef = ref(getDatabase(), `User/${username}/Age`);
+    const ageRef = ref(getDatabase(), `user/${username}/age`);
     set(ageRef, age);
 }
 
-// Function to write a user's display name
+// Function to write/change a user's display name
 export function writeUserDisplayName(username, displayName) {
-    const displayNameRef = ref(getDatabase(), `User/${username}/Display Name`);
+    const displayNameRef = ref(getDatabase(), `user/${username}/displayname`);
     set(displayNameRef, displayName);
 }
 
-// Function to write a user's email
+// Function to write/change a user's email
 export function writeUserEmail(username, email) {
-    const emailRef = ref(getDatabase(), `User/${username}/email`);
+    const emailRef = ref(getDatabase(), `user/${username}/email`);
     set(emailRef, email);
 }
 
-// Function to write an image URL to an album
-export function writeAlbumImageUrl(username, albumname, imageid, imageUrl) {
-    const imageUrlRef = ref(getDatabase(), `Album/${username}/${albumname}/${imageid}/imageurl`);
-    set(imageUrlRef, imageUrl);
+// ALBUM/IMAGE WRITE FUNCTIONS -----------------------------------------------------------------------------------------------------------------------------
+
+// Function to create/write a new album
+export function writeNewAlbum(username, albumname) {
+    const albumRef = ref(getDatabase(), `album/${username}/${albumname}`);
+    set(albumRef, {});
+}
+// maybe need error handling for this function if album already exists ↑
+
+// Function to write a new picture to an album
+export function writeNewPicture(username, albumname, imageuuid, imageurl, city, country, province) {
+    const pictureRef = ref(getDatabase(), `album/${username}/${albumname}/${imageuuid}`);
+    set(pictureRef, {
+        imageurl: imageurl,
+        info: {
+            city: city,
+            country: country,
+            province: province
+        }
+    });
 }
 
-// Function to write geoinfo to an image in an album
-export function writeImageGeoInfo(username, albumname, imageid, city, country, province) {
-    const geoInfoRef = ref(getDatabase(), `Album/${username}/${albumname}/${imageid}/info`);
-    set(geoInfoRef, { city, country, province });
-}
 
-writeUserAge('JonJonsson123', 30);
-writeUserDisplayName('JonJonsson123', 'Jón Jónsson');
-writeUserEmail('JonJonsson123', 'jonjonsson123@gmail.com');
-writeAlbumImageUrl('JonJonsson123', 'Album1', 'uuid', 'url to image');
-writeImageGeoInfo('JonJonsson123', 'Album1', 'uuid', 'city name', 'country name', 'province name');
+/*
+writeNewAlbum('jonjonsson123', 'an_album_name');
+writeNewPicture('jonjonsson123', 'an_album_name', 'anewuuid', 'new url to image', 'new city name', 'new country name', 'new province name');
+writeUserAge('jonjonsson123', 30);
+writeUserDisplayName('jonjonsson123', 'Jón Jónsson');
+writeUserEmail('jonjonsson123', 'jonjonsson123@gmail.com');
+*/
