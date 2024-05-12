ace7fd43c1d5c4b82fe69c40e862f1a23dc04b5e
ragnaragust <ragnarom21@tskoli.is>
2024-05-12 23:07:56 +0000

    FINISHED

---
+++ my-app/src/app/Auth/page.tsx
@@ -0,0 +1,28 @@
+// Page.tsx
+"use client"
+import React, { useState } from 'react';
+import Login from './login';
+import SignUp from './signUpPage';
+
+export default function Page() {
+    const [isSignUp, setIsSignUp] = useState(false);
+
+    const handleToggleForm = () => {
+        setIsSignUp((prev) => !prev);
+    };
+
+    return (
+        <div className="flex justify-center items-center h-screen bg-white">
+            <div className="p-4 flex flex-col items-center border border-black rounded-md bg-black">
+                {isSignUp ? (
+                    <SignUp />
+                ) : (
+                    <Login />
+                )}
+                <button onClick={handleToggleForm} className="mt-4 px-4 py-2 text-xl border rounded-md bg-white text-black hover:text-gray-200 active:text-gray-300 hover:px-5">
+                    {isSignUp ? "Switch to Login" : "Switch to Sign Up"}
+                </button>
+            </div>
+        </div>
+    );
+}
