3adc3e5ec8b46cc2c64379fa8a8a01985a066310
Valdas Kaubrys <valdaskaubrys9@gmail.com>
2024-05-12 17:44:42 +0000

    feat: Add Firebase authentication to login page

--- my-app/src/app/Login/page.tsx
+++ my-app/src/app/Login/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import React, { useState } from 'react';
+import { auth } from "../firebase/firebaseConfig"; 
 
 
 export default function LoginPage() {
--- my-app/src/app/firebase/firebaseConfig.js
+++ my-app/src/app/firebase/firebaseConfig.js
@@ -1,5 +1,8 @@
 const apiKey = process.env.FIREBASE_API_KEY;
 
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
 export const firebaseConfig = {
     apiKey:apiKey,
     authDomain: "verkefni-4-og-5-i-vefthroun.firebaseapp.com",
@@ -8,3 +11,7 @@ export const firebaseConfig = {
     appId: "1:769414348370:web:89ddd7a7f5f3721952698b",
     databaseURL: "https://verkefni-4-og-5-i-vefthroun-default-rtdb.firebaseio.com/"
 };
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
