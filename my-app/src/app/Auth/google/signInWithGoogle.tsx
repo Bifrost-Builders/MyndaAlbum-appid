5218f68e83525043ae641b61107c33e877e411df
GabrielMani <gabriel.sigvaldason@gmail.com>
2024-05-13 19:39:38 +0000

    quick fix google auth

--- my-app/src/app/Auth/google/signInWithGoogle.tsx
+++ my-app/src/app/Auth/google/signInWithGoogle.tsx
@@ -1,10 +1,13 @@
 "use client"
-import { signInWithPopup } from 'firebase/auth';
+import { signInWithPopup, onAuthStateChanged } from 'firebase/auth';
 import { auth, googleAuthProvider } from '../../firebase/firebaseConfig';
+import { useRouter } from 'next/navigation';
 import googleicon from "@/app/public/googleicon.png"
 import Image from 'next/image';
 
 const GoogleSignInButton = () => {
+  const router = useRouter();
+
   const handleSignInWithGoogle = async () => {
     try {
       await signInWithPopup(auth, googleAuthProvider);
@@ -12,21 +15,27 @@ const GoogleSignInButton = () => {
       console.error('Google Sign-in Error:', error);
     }
   };
-  return (
-      <button
-          onClick={handleSignInWithGoogle}
-          className="bg-slate-100 text-black hover:bg-slate-100 h-12 w-60 rounded-[5px] font-semibold text-lg flex justify-center items-center gap-x-3"
-        >
-          <Image
-              className="size-7"
-              alt='Google logo'
-              src={googleicon}
-              height={30}
-              width={30}
-          />
 
-          Sign in with Google
-      </button>
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      router.push('/secure');
+    }
+  });
+
+  return (
+    <button
+      onClick={handleSignInWithGoogle}
+      className="bg-slate-100 text-black hover:bg-slate-100 h-12 w-60 rounded-[5px] font-semibold text-lg flex justify-center items-center gap-x-3"
+    >
+      <Image
+        className="size-7"
+        alt='Google logo'
+        src={googleicon}
+        height={30}
+        width={30}
+      />
+      Sign in with Google
+    </button>
   );
 };
 
