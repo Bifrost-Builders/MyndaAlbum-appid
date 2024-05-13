7081a18b4f22f2f62c127469bd0767a4bff0636a
Valdas Kaubrys <valdaskaubrys9@gmail.com>
2024-05-13 13:17:46 +0000

    chore: Update react-router-dom to version 6.23.1

--- my-app/src/app/Auth/login.tsx
+++ my-app/src/app/Auth/login.tsx
@@ -1,15 +1,17 @@
-// LoginPage.tsx
 import React, { useState } from 'react';
+import { app } from '../firebase/firebaseConfig';
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
 export default function LoginPage() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
     const [errors, setErrors] = useState({
         email: '',
         password: '',
     });
 
-    const handleLogin = (e) => {
+    const handleLogin = async (e) => {
         e.preventDefault();
 
         const errorsCopy = { ...errors };
@@ -31,30 +33,42 @@ export default function LoginPage() {
 
         // If there are no errors, proceed with login
         if (!Object.values(errorsCopy).some((error) => error !== '')) {
-            // Add your login logic here
-            console.log('Email:', email);
-            console.log('Password:', password);
+            const auth = getAuth(app);
+            try {
+                await signInWithEmailAndPassword(auth, email, password);
+                setIsLoggedIn(true);
+                // You can redirect the user to another page upon successful login.
+            } catch (error) {
+                setErrors({email: error.message, password: error.message});
+            }
         }
     };
 
     return (
-        <div className="flex justify-center items-center bg-white">
-            <div className="border border-black rounded-md p-4 flex flex-col items-center">
-                <h2 className="text-white mb-6 text-3xl p-4 bg-black inline-block">Login Page</h2>
-                <form onSubmit={handleLogin} className="w-80">
-                    <div className="mb-4">
-                        <label className="block text-2xl">Email:</label>
-                        <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} className={`border rounded-md p-1 w-full ${errors.email ? 'border-red-500' : 'border-gray-300'}`} />
-                        {errors.email && <p className="text-red-500 text-sm">{errors.email}</p>}
-                    </div>
-                    <div className="mb-4">
-                        <label className="block text-2xl">Password:</label>
-                        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className={`border rounded-md p-1 w-full ${errors.password ? 'border-red-500' : 'border-gray-300'}`} />
-                        {errors.password && <p className="text-red-500 text-sm">{errors.password}</p>}
-                    </div>
-                    <button type="submit" className="px-4 py-2 text-2xl border rounded-md bg-black text-white hover:text-gray-200 active:text-gray-300 hover:px-5 w-full">Login</button>
-                </form>
-            </div>
+        <div className="flex justify-center items-center h-screen">
+            {!isLoggedIn ? (
+                <div className="text-center border border-black rounded-md p-10">
+                    <h2 className=" text-white mb-36 text-3xl p-10 border rounded-md bg-black">Login Page</h2>
+                    <form onSubmit={handleLogin}>
+                        <div className="mb-4">
+                            <label className="block text-2xl">Email:</label>
+                            <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} className={`border rounded-md p-1 w-full ${errors.email ? 'border-red-500' : 'border-gray-300'}`} />
+                            {errors.email && <p className="text-red-500 text-sm">{errors.email}</p>}
+                        </div>
+                        <div className="mb-4">
+                            <label className="block text-2xl">Password:</label>
+                            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className={`border rounded-md p-1 w-full ${errors.password ? 'border-red-500' : 'border-gray-300'}`} />
+                            {errors.password && <p className="text-red-500 text-sm">{errors.password}</p>}
+                        </div>
+                        <button type="submit" className="px-4 py-2 text-2xl border rounded-md bg-black text-white hover:text-gray-200 active:text-gray-300 hover:px-5">Login</button>
+                    </form>
+                </div>
+            ) : (
+                <div>
+                    <h2 className='text-white'>Welcome, {email}!</h2>
+                    {/* You can render the content for logged-in users here */}
+                </div>
+            )}
         </div>
     );
 }
