ea8a90953c170f4e3ca1c1adebcedce26e7a4fae
Valdas Kaubrys <valdaskaubrys9@gmail.com>
2024-05-14 19:05:29 +0000

    chore: Remove unused imports and comments in secure/page.tsx

--- my-app/src/app/secure/page.tsx
+++ my-app/src/app/secure/page.tsx
@@ -1,11 +1,11 @@
 "use client";
 import LeftSideNav from '@/app/components/userUi/leftSideNav'
 import HandleImage from "@/app/components/userUi/imageUi/handleImage"
-//import blabla from "@/app/components/userUi/imageUi/showImages"
+//import blabla from "@/app/components/userUi/imageUi/showImages" commenta ut þvi að gaf villur þegar reyndi að builda þetta á hýsingunni
 import Image from "next/image"
 import ShowImages from '@/app/components/userUi/imageUi/showImages'
 import { useEffect, useState } from 'react';
-//import BlaBla from '@/app/components/userUi/imageUi/homePage'
+//import BlaBla from '@/app/components/userUi/imageUi/homePage' commenta ut þvi að gaf villur þegar reyndi að builda þetta á hýsingunni
 //Use client ekki gott!
 import Link from 'next/link'
 import undraw_Upload_image_re_svxx from "@/app/public/undraw_Upload_image_re_svxx.png"
@@ -34,10 +34,10 @@ import { readFromFirebase } from '../lib/scripts'
 import { MobileSideBar } from '../components/header/navSmall/sideBar';
 import { useCycle } from 'framer-motion';
 import { getAuth, onAuthStateChanged,signOut } from 'firebase/auth';
-import { useRouter } from 'next/navigation';
+import { useRouter } from 'next/router';
 
 
-export default function homePage() {
+export default function HomePage() {
     const router = useRouter();
     const [bgMode, cycleBgMode] = useCycle("white", "black");
     const [bgCard, cycleBgCard] = useCycle("black", "blue-600")
